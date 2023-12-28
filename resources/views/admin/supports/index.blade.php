f48a0235d2716422cdeb204a5b36691213a26365
Vitor Nunes da Silva <vitor1nuness@gmail.com>
2023-12-28 18:19:01 -0300

    Aula 35 UI/UX com TailwindCss

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,29 +1,36 @@
-<h1>Listagem dos suportes</h1>
+@extends('admin/layouts/app')
 
-<a href="{{ route('supports.create') }}">Cria Dúvida</a>
+@section('title', 'Supports')
 
-<table>
-    <thead>
-        <th>assunto</th>
-        <th>status</th>
-        <th>descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">ir</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('header')
+    <h1>Listagem dos Suportes</h1>
+@endsection
 
-<x-pagination
-    :paginator="$supports"
-    :appends="$filters" />
+@section('content')
+
+    <a href="{{ route('supports.create') }}">Cria Dúvida</a>
+
+    <table>
+        <thead>
+            <th>assunto</th>
+            <th>status</th>
+            <th>descrição</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @foreach ($supports->items() as $support)
+                <tr>
+                    <td>{{ $support->subject }}</td>
+                    <td>{{ getStatusSupport($support->status) }}</td>
+                    <td>{{ $support->body }}</td>
+                    <td>
+                        <a href="{{ route('supports.show', $support->id) }}">ir</a>
+                        <a href="{{ route('supports.edit', $support->id) }}">editar</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <x-pagination :paginator="$supports" :appends="$filters" />
+@endsection
